8ad8d1dd9966c2c48bb14a8054d56c2d0f8dcc76
Sasha T Heyburgh <sasha.heyburgh@gmail.com>
2026-04-01 16:05:26 +0200

    Added intent for activities

--- app/src/main/java/com/example/multiplytable/TableDisplay.kt
+++ app/src/main/java/com/example/multiplytable/TableDisplay.kt
@@ -1,25 +1,16 @@
 package com.example.multiplytable
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class TableDisplay : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val multiplyBtn = findViewById<Button>(R.id.multiplyBtn)
-        multiplyBtn.setOnClickListener {
-            val intent = Intent(this, TableDisplay::class.java)
-            startActivity(intent)
-        }
-
+        setContentView(R.layout.activity_table_display)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
